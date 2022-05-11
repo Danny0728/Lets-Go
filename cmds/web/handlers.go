ba41d1222b045060c135bba5ab44492ce1ad32f8
Yash Chavan <yashdhoni0728@gmail.com>
2022-05-12 01:30:00 +0530

    Updated snippetbox with third party router and used RESTful routing structure

--- cmds/web/handlers.go
+++ cmds/web/handlers.go
@@ -11,23 +11,20 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w) //use of notFound helper
-		return
-	}
-
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	app.render(w, r, "home.page.tmpl", &templateData{Snippets: snippets})
+	app.render(w, r, "home.page.tmpl", &templateData{
+		Snippets: snippets,
+	})
 }
 
 //add a showsnippet handler function
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -42,17 +39,17 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.render(w, r, "show.page.tmpl", &templateData{Snippet: snippet})
+	app.render(w, r, "show.page.tmpl", &templateData{
+		Snippet: snippet,
+	})
+}
+
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a Snippet Form"))
 }
 
 //add a createSnippet handler function
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		w.Header()["Content-Length"] = nil
-		app.clientError(w, http.StatusMethodNotAllowed) //this sends the response to the user for us
-		return
-	}
 	//creating some dummy builds we are going to delete this later
 	title := "hello"
 	content := "Hello is a nice word"

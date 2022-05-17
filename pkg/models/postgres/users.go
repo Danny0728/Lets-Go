4ee99a2585075901229e58e50f02f54408d221bd
Yash Chavan <yashdhoni0728@gmail.com>
2022-05-17 22:24:32 +0530

    Added Request Context

--- pkg/models/postgres/users.go
+++ pkg/models/postgres/users.go
@@ -43,5 +43,15 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 	return id, nil
 }
 func (m *UserModel) Get(id int) (*models.User, error) {
-	return nil, nil
+	s := &models.User{}
+
+	sqlQuery := `SELECT id,name,email, created FROM users WHERE id=$1`
+	err := m.DB.QueryRow(sqlQuery, id).Scan(&s.ID, &s.Name, &s.Email, &s.Created)
+
+	if err == sql.ErrNoRows {
+		return nil, models.ErrNoRecord
+	} else if err != nil {
+		return nil, err
+	}
+	return s, nil
 }
